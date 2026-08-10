099329f3e23e1300fe0f4f721929b080c7a456eb
AiNet-999 <research.ainet@gmail.com>
2026-08-10 09:32:05 +0500

    Update MSTCA_ResNet_Knee.py

--- Models/MSTCA_ResNet_Knee.py
+++ Models/MSTCA_ResNet_Knee.py
@@ -7,51 +7,51 @@ import seaborn as sns
 import pandas as pd
 import random
 import warnings
-import pandas as pd
-from sklearn.model_selection import train_test_split, KFold
+
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import (
-    accuracy_score, precision_score, recall_score, f1_score, matthews_corrcoef,
-    roc_curve, roc_auc_score, confusion_matrix, classification_report
+    accuracy_score, precision_score, recall_score, f1_score,
+    matthews_corrcoef, roc_curve, roc_auc_score, confusion_matrix,
+    classification_report, log_loss
 )
 
 import tensorflow as tf
 from tensorflow.keras import backend as K
-from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.callbacks import EarlyStopping, CSVLogger
 from tensorflow.keras.layers import (
     Input, Conv2D, MaxPooling2D, Flatten, Dense, Dropout, Reshape,
-    LeakyReLU, GlobalAveragePooling2D, Multiply, Layer, MultiHeadAttention,
-    LayerNormalization, BatchNormalization, Add, Concatenate,SpatialDropout2D
+    LeakyReLU, GlobalAveragePooling2D, GlobalAveragePooling1D, Multiply,
+    Layer, MultiHeadAttention, LayerNormalization, Add, Concatenate,
+    BatchNormalization
 )
-
 from tensorflow.keras.models import Model
-from tensorflow.keras.callbacks import CSVLogger
 
-from tensorflow.keras.regularizers import l2
 warnings.filterwarnings('ignore')
 
+# https://github.com/kobiso/CBAM-keras
+# https://github.com/titu1994/keras-squeeze-excite-network
 
-#https://github.com/kobiso/CBAM-keras
-#https://github.com/titu1994/keras-squeeze-excite-network
+class DenseBlock(Layer):
+    def __init__(self, num_layers, growth_rate, kernel_size=3):
+        super(DenseBlock, self).__init__()
+        self.num_layers = num_layers
+        self.growth_rate = growth_rate
+        self.kernel_size = kernel_size
+        self.conv_layers = [
+            Conv2D(growth_rate, kernel_size=kernel_size, padding='same') 
+            for _ in range(num_layers)
+        ]
 
-def ResidualBlock(filters):
-    def block(x_input):
-        x = Conv2D(filters, (3, 3), padding='same', kernel_regularizer=l2(1e-4))(x_input)
-        x = BatchNormalization()(x)
-        x = LeakyReLU(alpha=0.05)(x)
-        x = SpatialDropout2D(0.2)(x)
-
-        x = Conv2D(filters, (3, 3), padding='same', kernel_regularizer=l2(1e-4))(x)
-        x = BatchNormalization()(x)
-
-        # If number of filters doesn't match, project input
-        if x_input.shape[-1] != filters:
-            x_input = Conv2D(filters, (1,1), padding='same', kernel_regularizer=l2(1e-4))(x_input)
-            x_input = BatchNormalization()(x_input)
-
-        x = Add()([x, x_input])
-        x = LeakyReLU(alpha=0.05)(x)
+    def call(self, inputs):
+        x = inputs
+        outputs = [x]
+        for conv in self.conv_layers:
+            x = conv(x)
+            x = LeakyReLU(alpha=0.05)(x)
+            outputs.append(x)
+            x = Concatenate(axis=-1)(outputs)
         return x
-    return block
+
 
 class AttentionModule(Layer):
     def __init__(self, num_heads):
@@ -61,28 +61,120 @@ class AttentionModule(Layer):
 
     def call(self, patches):
         attn_output = self.attention(patches, patches)
-        return self.layer_norm(attn_output + patches)  
+        return self.layer_norm(attn_output + patches)
 
-class SEBlock(Layer):
-    def __init__(self, ratio=16):
-        super(SEBlock, self).__init__()
-        self.ratio = ratio
-        self.global_avg_pool = GlobalAveragePooling2D()
-        self.fc1 = None  # Will initialize later
-        self.fc2 = None
+
+class ResNetBlock(Layer):
+    def __init__(self, filters, stride=1, **kwargs):
+        super().__init__(**kwargs)
+        self.filters = filters
+        self.stride = stride
+        self.conv1 = Conv2D(filters, 3, strides=stride, padding='same', use_bias=False)
+        self.bn1 = LayerNormalization()
+        self.act1 = LeakyReLU(0.05)
+        self.conv2 = Conv2D(filters, 3, padding='same', use_bias=False)
+        self.bn2 = LayerNormalization()
+        self.shortcut = None
 
     def build(self, input_shape):
-        channels = input_shape[-1]
-        self.fc1 = Dense(channels // self.ratio, activation='relu')
-        self.fc2 = Dense(channels, activation='sigmoid')
-        self.reshape = Reshape((1, 1, channels))
+        in_channels = input_shape[-1]
+        if in_channels != self.filters or self.stride != 1:
+            self.shortcut = tf.keras.Sequential([
+                Conv2D(self.filters, 1, strides=self.stride, padding='same', use_bias=False),
+                BatchNormalization()
+            ])
+        else:
+            self.shortcut = lambda x: x
+        super().build(input_shape)
 
-    def call(self, inputs):
-        se = self.global_avg_pool(inputs)
-        se = self.fc1(se)
-        se = self.fc2(se)
-        se = self.reshape(se)
-        return Multiply()([inputs, se])
+    def call(self, x):
+        shortcut = self.shortcut(x)
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.act1(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = Add()([x, shortcut])
+        x = LeakyReLU(0.05)(x)
+        return x
+
+
+class SEBlock(Layer):
+    def __init__(self, ratio=16, **kwargs):
+        super().__init__(**kwargs)
+        self.ratio = ratio
+
+    def build(self, input_shape):
+        c = input_shape[-1]
+        self.gap = GlobalAveragePooling2D()
+        self.fc1 = Dense(max(c // self.ratio, 8), activation='relu')
+        self.fc2 = Dense(c, activation='sigmoid')
+        self.reshape = Reshape((1, 1, c))
+        super().build(input_shape)
+
+    def call(self, x):
+        s = self.gap(x)
+        s = self.fc1(s)
+        s = self.fc2(s)
+        s = self.reshape(s)
+        return Multiply()([x, s])
+
+
+class MultiScalePatchEmbedding(Layer):
+    def __init__(self, embed_dim=128, patch_sizes=[8, 16, 32], **kwargs):
+        super().__init__(**kwargs)
+        self.embed_dim = embed_dim
+        self.patch_sizes = patch_sizes
+        self.proj_layers = [
+            Conv2D(embed_dim, kernel_size=p, strides=p, padding='valid')
+            for p in patch_sizes
+        ]
+        self.fuse = Dense(embed_dim)
+
+    def call(self, x):
+        multi_feats = []
+        for conv in self.proj_layers:
+            f = conv(x)
+            h = tf.shape(f)[1]
+            w = tf.shape(f)[2]
+            f = tf.reshape(f, (-1, h * w, tf.shape(f)[-1]))
+            multi_feats.append(f)
+        x = Concatenate(axis=1)(multi_feats)
+        x = self.fuse(x)
+        return x
+
+
+class TransformerBlock(Layer):
+    def __init__(self, embed_dim=128, num_heads=4, **kwargs):
+        super().__init__(**kwargs)
+        self.attn = MultiHeadAttention(num_heads=num_heads, key_dim=embed_dim)
+        self.norm1 = LayerNormalization()
+        self.norm2 = LayerNormalization()
+        self.ffn = tf.keras.Sequential([
+            Dense(embed_dim * 2, activation='gelu'),
+            Dense(embed_dim)
+        ])
+
+    def call(self, x):
+        a = self.attn(x, x)
+        x = self.norm1(x + a)
+        f = self.ffn(x)
+        x = self.norm2(x + f)
+        return x
+
+
+class CrossAttentionFusion(Layer):
+    def __init__(self, embed_dim=128, num_heads=4, **kwargs):
+        super().__init__(**kwargs)
+        self.attn = MultiHeadAttention(num_heads=num_heads, key_dim=embed_dim)
+        self.norm = LayerNormalization()
+
+    def call(self, cnn_feat, vit_feat):
+        cnn_feat = tf.expand_dims(cnn_feat, axis=1)
+        vit_feat = tf.expand_dims(vit_feat, axis=1)
+        attn_out = self.attn(query=cnn_feat, key=vit_feat, value=vit_feat)
+        fused = self.norm(attn_out + cnn_feat)
+        return tf.squeeze(fused, axis=1)
 
 
 def load_and_preprocess_images(directory, label, img_rows=512, img_cols=512):
@@ -99,16 +191,11 @@ def load_and_preprocess_images(directory, label, img_rows=512, img_cols=512):
 
 
 
-X_train, Y_train = [], []
-X_test, Y_test = [], []
 X_data, Y_data = [], []
-X_train, Y_train = [], []
-X_test, Y_test = [], []
 
 data_dirs = {
     '/kaggle/input/kneemeddataset/KneeMedDataset/deepfake': 0,
     '/kaggle/input/kneemeddataset/KneeMedDataset/real': 1
-
 }
 
 for directory, label in data_dirs.items():
@@ -116,12 +203,10 @@ for directory, label in data_dirs.items():
     X_data.extend(images)
     Y_data.extend(labels)
 
-
 X_data = np.array(X_data)
 Y_data = np.array(Y_data)
 
-
-img_rows, img_cols = 224,224
+img_rows, img_cols = 224, 224
 if tf.keras.backend.image_data_format() == 'channels_first':
     X_data = X_data.reshape(X_data.shape[0], 3, img_rows, img_cols)
     input_shape = (3, img_rows, img_cols)
@@ -131,177 +216,112 @@ else:
 
 X_data = X_data.astype('float32') / 255.0
 
-from sklearn.model_selection import train_test_split  
-X_train, X_test, Y_train, Y_test = train_test_split(X_data, Y_data, test_size=0.2, random_state=4,stratify=Y_data)
+
+X_train_full, X_test, Y_train_full, Y_test = train_test_split(
+    X_data, Y_data, test_size=0.2, random_state=4, stratify=Y_data
+)
+
+X_train, X_val, Y_train, Y_val = train_test_split(
+    X_train_full, Y_train_full, test_size=0.1, random_state=42, stratify=Y_train_full
+)
 
 print(f"Training data shape: {X_train.shape}")
+print(f"Validation data shape: {X_val.shape}")
 print(f"Testing data shape: {X_test.shape}")
 
 
-
-
-
-class MultiScalePatches(Layer):
-    def __init__(self, patch_sizes, projection_dim):
-        super(MultiScalePatches, self).__init__()
-        self.patch_sizes = patch_sizes
-        self.projection_dim = projection_dim
-        self.projection_layers = [Dense(projection_dim) for _ in patch_sizes]
-
-    def call(self, images):
-        batch_size = tf.shape(images)[0]
-        patches_list = []
-        for i, patch_size in enumerate(self.patch_sizes):
-            patches = tf.image.extract_patches(
-                images=images,
-                sizes=[1, patch_size, patch_size, 1],
-                strides=[1, patch_size, patch_size, 1],
-                rates=[1, 1, 1, 1],
-                padding="VALID",
-            )
-            patch_dims = patches.shape[-1]
-            patches = tf.reshape(patches, [batch_size, -1, patch_dims])
-            projected_patches = self.projection_layers[i](patches)  # Project to the same dimension
-            patches_list.append(projected_patches)
-        return tf.concat(patches_list, axis=1)
-
-def get_hybrid_model(input_shape):
-    input_img = Input(input_shape)
-
-    patch_sizes = [16, 32, 48]
-    projection_dim = 64
-    patches = MultiScalePatches(patch_sizes, projection_dim)(input_img)
-
-    attn_output = AttentionModule(num_heads=8)(patches)
-
-    num_patches = sum([(input_shape[0] // patch_size) * (input_shape[1] // patch_size) for patch_size in patch_sizes])
-    patch_depth = projection_dim
-
-    attn_output = Reshape((num_patches, patch_depth))(attn_output)
-    x = Reshape((num_patches, patch_depth, 1))(attn_output)
-    x = ResidualBlock(32)(x)
-    x = SEBlock()(x)  
-    x = MaxPooling2D(2, 2)(x)
-
-    x = ResidualBlock(64)(x)
-    x = SEBlock()(x)  
-    x = MaxPooling2D(2, 2)(x)
-
-    x = ResidualBlock(128)(x)
-    x = SEBlock()(x) 
-    x = MaxPooling2D(2, 2)(x)
-
-    x = ResidualBlock(256)(x)
-    x = SEBlock()(x)  
-    x = MaxPooling2D(2, 2)(x)
+def get_hybrid_modelResNet(input_shape):
+    inputs = Input(shape=input_shape)
     
-    x = Flatten()(x)
-    x = Dense(512)(x)
-    x = LeakyReLU(alpha=0.05)(x)
-    x = Dropout(0.05)(x)
-    x = Dense(1, activation='sigmoid')(x)
-    model = Model(inputs=[input_img], outputs=[x])
+    # CNN Branch
+    cnn = Conv2D(32, 3, padding='same', activation='relu')(inputs)
+    cnn = ResNetBlock(32)(cnn)
+    cnn = ResNetBlock(32)(cnn)
+    cnn = SEBlock()(cnn)
+    cnn = MaxPooling2D(2)(cnn)
+
+    cnn = ResNetBlock(64)(cnn)
+    cnn = ResNetBlock(64)(cnn)
+    cnn = SEBlock()(cnn)
+    cnn = MaxPooling2D(2)(cnn)
+
+    cnn = Conv2D(128, 3, padding='same', activation='relu')(cnn)
+    cnn = GlobalAveragePooling2D()(cnn)
+
+    # ViT Branch
+    vit = MultiScalePatchEmbedding(embed_dim=128, patch_sizes=[8, 16, 32])(inputs)
+    vit = TransformerBlock(embed_dim=128, num_heads=4)(vit)
+    vit = TransformerBlock(embed_dim=128, num_heads=4)(vit)
+    vit = GlobalAveragePooling1D()(vit)
+
+    # Fusion
+    fusion = CrossAttentionFusion(embed_dim=128, num_heads=4)(cnn, vit)
+    fusion = Concatenate()([fusion, cnn, vit])
+    fusion = Dense(256, activation='relu')(fusion)
+    fusion = Dropout(0.3)(fusion)
+    fusion = Dense(128, activation='relu')(fusion)
+    fusion = Dropout(0.2)(fusion)
+
+    outputs = Dense(1, activation='sigmoid')(fusion)
+    model = Model(inputs, outputs, name="Hybrid_Model_ResNet")
     return model
 
- 
 
 def compute_eer(y_true, y_scores):
     fpr, tpr, thresholds = roc_curve(y_true, y_scores)
     fnr = 1 - tpr
-    eer_threshold = thresholds[np.nanargmin(np.abs(fnr - fpr))]
     eer = fnr[np.nanargmin(np.abs(fnr - fpr))]
     return eer
 
 
-num_folds = 3
-kf = KFold(n_splits=num_folds, shuffle=True, random_state=42)
-metrics = {
-    'accuracy': [], 'precision': [], 'recall': [], 'f1_score': [],
-    'eer': [], 'mcc': []
-}
-
-
+# Callbacks
 early_stopping = EarlyStopping(
-    monitor='val_accuracy', 
+    monitor='val_accuracy',
     patience=5,
     restore_best_weights=True
 )
-from sklearn.metrics import (
-    accuracy_score, precision_score, recall_score, f1_score, matthews_corrcoef, roc_curve
+csv_logger = CSVLogger('training_history.csv', append=False)
+
+# Model Training
+model = get_hybrid_modelResNet(input_shape)
+model.compile(
+    optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001),
+    loss='binary_crossentropy',
+    metrics=['accuracy']
 )
-for fold, (train_index, test_index) in enumerate(kf.split(X_data)):
-    print(f"\nTraining fold {fold + 1}/{num_folds}...")
 
+history = model.fit(
+    X_train, Y_train,
+    batch_size=16,
+    epochs=30,
+    validation_data=(X_val, Y_val),
+    shuffle=True,
+    callbacks=[early_stopping, csv_logger]
+)
 
-    X_train_full, X_test = X_data[train_index], X_data[test_index]
-    Y_train_full, Y_test = Y_data[train_index], Y_data[test_index]
+# Evaluation
+y_pred_proba = model.predict(X_test).flatten()
+y_pred = (y_pred_proba > 0.5).astype(int)
 
-    X_train, X_val, Y_train, Y_val = train_test_split(X_train_full, Y_train_full, test_size=0.1, random_state=42)
+accuracy = accuracy_score(Y_test, y_pred)
+precision = precision_score(Y_test, y_pred)
+recall = recall_score(Y_test, y_pred)
+f1 = f1_score(Y_test, y_pred)
+eer = compute_eer(Y_test, y_pred_proba)
+mcc = matthews_corrcoef(Y_test, y_pred)
 
-    
-    model = get_hybrid_model(input_shape)
-    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001), 
-                  loss='binary_crossentropy', 
-                  metrics=['accuracy'])
-    csv_logger = CSVLogger(f'training_history_fold_{fold + 1}.csv', append=False)
+print("\nEvaluation Metrics:")
+print(f"Accuracy:  {accuracy:.4f}")
+print(f"Precision: {precision:.4f}")
+print(f"Recall:    {recall:.4f}")
+print(f"F1-Score:  {f1:.4f}")
+print(f"EER:       {eer:.4f}")
+print(f"MCC:       {mcc:.4f}\n")
 
-   
-    history=model.fit(
-        X_train, Y_train, 
-        batch_size=16, 
-        epochs=30, 
-        validation_data=(X_val, Y_val), 
-        shuffle=True,
-          callbacks=[early_stopping, csv_logger]  
-    )
+class_report = classification_report(Y_test, y_pred, target_names=['Deepfake', 'Real'])
+print("Classification Report:\n", class_report)
 
-  
-    y_pred_proba = model.predict(X_test).flatten()
-    y_pred = (y_pred_proba > 0.5).astype(int)
-    
-
-    accuracy = accuracy_score(Y_test, y_pred)
-    precision = precision_score(Y_test, y_pred)
-    recall = recall_score(Y_test, y_pred)
-    f1 = f1_score(Y_test, y_pred)
-    eer = compute_eer(Y_test, y_pred_proba)
-    mcc = matthews_corrcoef(Y_test, y_pred)
-    
-   
-    metrics['accuracy'].append(accuracy)
-    metrics['precision'].append(precision)
-    metrics['recall'].append(recall)
-    metrics['f1_score'].append(f1)
-    metrics['eer'].append(eer)
-    metrics['mcc'].append(mcc)
-
-   
-    print(f"Fold {fold + 1} Metrics:")
-    print(f"Accuracy: {accuracy:.4f}, Precision: {precision:.4f}, Recall: {recall:.4f}, F1-Score: {f1:.4f}, EER: {eer:.4f}, MCC: {mcc:.4f}")
-    results = model.predict(X_test)
-    predicted_probabilities = results.flatten()  
-    from sklearn.metrics import (
-    roc_curve, 
-    roc_auc_score, 
-    confusion_matrix, 
-    classification_report, 
-    accuracy_score, 
-    log_loss,
-    matthews_corrcoef)
-    class_report = classification_report(Y_test, (predicted_probabilities > 0.5).astype(int), target_names=['Deepfake', 'Real'])
-    print("Classification Report:\n", class_report)
-    
-
-
-print("\nOverall Metrics (Mean ± Std):")
-for metric, values in metrics.items():
-    mean = np.mean(values)
-    std_dev = np.std(values)
-    print(f"{metric.capitalize()}: {mean:.4f} ± {std_dev:.4f}")
-
-
-
-
+# Confusion Matrix Plot
 cm = confusion_matrix(Y_test, y_pred)
 cm_sum = np.sum(cm, axis=1, keepdims=True)
 cm_perc = cm / cm_sum.astype(float) * 100
@@ -314,14 +334,14 @@ for i in range(nrows):
         p = cm_perc[i, j]
         annot[i, j] = f'{c}\n({p:.1f}%)'
 
-# Plotting
 plt.figure(figsize=(6, 5))
-sns.heatmap(cm, annot=annot, fmt='', cmap='BuPu', 
-            xticklabels=['Deepfake', 'Real'], 
-            yticklabels=['Deepfake', 'Real'], 
-            linewidths=1.5, linecolor='black', cbar=True,
-            annot_kws={"size": 13, "weight": "bold"})
-
+sns.heatmap(
+    cm, annot=annot, fmt='', cmap='BuPu',
+    xticklabels=['Deepfake', 'Real'],
+    yticklabels=['Deepfake', 'Real'],
+    linewidths=1.5, linecolor='black', cbar=True,
+    annot_kws={"size": 13, "weight": "bold"}
+)
 plt.xlabel('Predicted Class', fontsize=12, weight='bold')
 plt.ylabel('Actual Class', fontsize=12, weight='bold')
 plt.title('Confusion Matrix', fontsize=14, weight='bold')
@@ -329,10 +349,9 @@ plt.savefig("Confusion matrix.png")
 plt.tight_layout()
 plt.show()
 
-
-
-fpr, tpr, thresholds = roc_curve(Y_test, predicted_probabilities)
-roc_auc = roc_auc_score(Y_test, predicted_probabilities)
+# ROC Curve Plot
+fpr, tpr, thresholds = roc_curve(Y_test, y_pred_proba)
+roc_auc = roc_auc_score(Y_test, y_pred_proba)
 
 fig, ax = plt.subplots(figsize=(5, 4), constrained_layout=True)
 ax.plot(fpr, tpr, color='blue', lw=2, label='ROC curve (area = %0.5f)' % roc_auc)
@@ -349,8 +368,7 @@ ax.grid(True)
 plt.savefig("ROC.png")
 plt.show()
 
-
-import matplotlib.pyplot as plt  
+# Training Curves Plot
 plt.style.use('bmh')
 plt.figure(figsize=(16, 6))
 
@@ -362,7 +380,7 @@ plt.xlabel('Epochs', fontsize=18)
 plt.ylabel('Accuracy', fontsize=18)
 plt.xticks(fontsize=18)
 plt.yticks(fontsize=18)
-plt.ylim(0, 1.1) 
+plt.ylim(0, 1.1)
 plt.grid(linewidth=2)
 plt.legend(fontsize=18)
 
@@ -376,7 +394,6 @@ plt.xticks(fontsize=18)
 plt.yticks(fontsize=18)
 plt.grid(linewidth=2)
 plt.legend(fontsize=18)
-
 
 plt.savefig('training_validation_curves1.png', dpi=1000, bbox_inches='tight')
 plt.show()
